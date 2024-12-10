cf193194e276ac2a1150e0ede1dc666d96b03435
Dylan Stitt <dstitt527@gmail.com>
2024-12-10 12:51:18 -0500

    init

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 # Dylan Stitt
 # Unit 4 Lab 3
-# [NAME]
+# Reverse a File
 
 from Stack import ArrayStack
+from string import ascii_lowercase, ascii_uppercase
+
+def clean(content):
+    newContent = []
+    for line in content:
+        newLine = []
+        for word in line.split(' '):
+            newWord = ""
+            for char in word:
+                if char in ascii_lowercase+ascii_uppercase:
+                    newWord += char
+            newLine.append(newWord)
+        newContent.append(' '.join(newLine))
+    return newContent
 
 def getContent(filename):
     with open(filename, 'r') as file:
-        return file.read().split(' ')
+        return clean(file.read().splitlines())
+
 
 def main():
     stack = ArrayStack()
-    content = getContent('test.txt')
+    content = getContent('earnest.txt')
 
-    for word in content:
-        stack.push(word)
+    for line in content:
+        if len(line) > 1:
+            stack.push(line)
 
+    new = ""
     for i in range(len(stack)):
-        print(stack.pop(), end=' ')
+        new += stack.pop() + ' '
+    with open('output.txt', 'w') as file:
+        file.write(new)
 
 
 if __name__ == '__main__':
